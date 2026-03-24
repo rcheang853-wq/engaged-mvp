0ff4c82df23abec8cb917d0c3eaca781f6e57210
rcheang853-qw <rcheang853@gmail.com>
2026-03-24 21:36:11 +0800

    chore(sentry): move client init to instrumentation-client

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,4 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
-import './sentry.client.config';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 0.1,
+  debug: false,
+});
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
